d784feff124f1fab81bd83156cbc41387f0b45d6
Amirmohammad Zarif Shahsavan Nejad <Developerboys@gmail.com>
2023-11-07 17:49:23 +0330

    fix: removed serial2

--- Control/HardwareSerial.ino
+++ Control/HardwareSerial.ino
@@ -1,8 +1,6 @@
 // Pin definitions for hardware serial ports
 const int serialPort1RxPin = 2;    // RX pin for Serial Port 1
 const int serialPort1TxPin = 3;    // TX pin for Serial Port 1
-const int serialPort2RxPin = 10;   // RX pin for Serial Port 2
-const int serialPort2TxPin = 11;   // TX pin for Serial Port 2
 
 void setup() {
   // Initialize Serial Port 1
@@ -17,10 +15,6 @@ void setup() {
   Serial1.setTimeout(100);             // Set timeout value if needed
   Serial1.setParity(UART_PARITY_NONE); // Set parity if needed
 
-  // Set the baud rate and other settings for Serial Port 2
-  Serial2.begin(9600);
-  Serial2.setTimeout(100);             // Set timeout value if needed
-  Serial2.setParity(UART_PARITY_NONE); // Set parity if needed
 }
 
 void loop() {
@@ -29,25 +23,13 @@ void loop() {
     // Read data from Serial Port 1
     char data = Serial1.read();
 
-    // Process the received data
-    // ...
   }
 
-  // Serial Port 2 communication
-  if (Serial2.available()) {
-    // Read data from Serial Port 2
-    char data = Serial2.read();
-
-    // Process the received data
-    // ...
-  }
 
   // Default hardware serial communication
   if (Serial.available()) {
     // Read data from the default hardware serial port
     char data = Serial.read();
 
-    // Process the received data
-    // ...
   }
 }
